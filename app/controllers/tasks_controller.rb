ce7b50002df13265d9b1f63a39a5ed91dd894b46
Masato Lee <masaroad15@gmail.com>
2018-01-10 13:16:20 +0000

    tasklist login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,10 @@
 class TasksController < ApplicationController
+  before_action :correct_user, only: [:show, :create, :edit, :update, :destroy]
 
   # 一覧画面
   def index
-    @tasks = Task.all.page(params[:page])
+    # 全件 → ログインしている人の全件
+    @tasks = current_user.tasks.all.page(params[:page])
   end
 
   def show
@@ -10,11 +12,13 @@ class TasksController < ApplicationController
   end
   
   def new
-    @task = Task.new
+    # 「ログインしている人の」「タスクを」「新規作成する」
+    # Task → current_user.tasks
+    @task = current_user.tasks.new
   end
   
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.new(task_params)
     
     if @task.save
       flash[:success] = 'タスクが正常に投稿されました'
@@ -56,4 +60,12 @@ end
   # Strong Parameter
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,20 @@
 Rails.application.routes.draw do
-  root to: 'tasks#index'
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
+  get 'toppages/index'
+
+  root to: 'toppages#index'
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+  get 'signup', to: 'users#new'
   
   resources :tasks
+  resources :users
 end
